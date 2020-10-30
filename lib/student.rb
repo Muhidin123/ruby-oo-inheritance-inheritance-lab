ba8df2a39d5b525cd4eac3290215ad092380b41c
Muhidin <muhidinhukic1@gmail.com>
2020-10-30 18:18:40 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
 
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize()
+        @knowledge = []
+        #super(knowledge)
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+   
 end
